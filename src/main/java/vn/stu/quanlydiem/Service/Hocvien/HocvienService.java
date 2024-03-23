782eacfe7f6dc34d6b1e88d3c4ffc94e0ba578f1
hoanghuan1682002@gmail.com <hoanghuan1682002@gmail.com>
2024-03-23 22:41:00 +0700

    new

--- src/main/java/vn/stu/quanlydiem/Service/Hocvien/HocvienService.java
+++ src/main/java/vn/stu/quanlydiem/Service/Hocvien/HocvienService.java
@@ -3,8 +3,17 @@ package vn.stu.quanlydiem.Service.Hocvien;
 import vn.stu.quanlydiem.Model.SinhVien;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HocvienService {
 
     List<SinhVien> findAll();
+
+    Optional<SinhVien> findById(Integer integer);
+
+    SinhVien save(SinhVien sv);
+
+    void deleteById(Integer integer);
+
+    SinhVien updateSv(Integer id, SinhVien sv);
 }
--- src/main/java/vn/stu/quanlydiem/Service/MonHoc/MonHocService.java
+++ src/main/java/vn/stu/quanlydiem/Service/MonHoc/MonHocService.java
@@ -4,8 +4,17 @@ import vn.stu.quanlydiem.Model.Monhoc;
 import vn.stu.quanlydiem.Model.SinhVien;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MonHocService {
 
     List<Monhoc> getAll();
+
+    Monhoc  save(Monhoc monhoc);
+
+    Optional<Monhoc> findById(Integer integer);
+
+    void deleteById(Integer integer);
+
+    Monhoc updateMonHoc(Integer id, Monhoc monhoc);
 }
